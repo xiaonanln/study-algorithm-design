2e238fdb6ecdf2c9b2fe734bf10e9ab90956642a
seis <xiaonanln@gmail.com>
2017-08-30 09:50:06 +0800

    HeapSort

--- src/ch4/HeapSort.py
+++ src/ch4/HeapSort.py
@@ -1,4 +1,3 @@
-
 class HeapSort(object):
 	def __init__(self, list):
 		self.list = list
@@ -6,16 +5,15 @@ class HeapSort(object):
 	def sort(self):
 		list = self.list
 		heapify(list)
-		for i in xrange(len(list)-1, 0, -1): # from len(list) ~ 1
-			list[i], list[0] = list[0], list[i] # swap list[i] and list[0]
+		for i in xrange(len(list) - 1, 0, -1):  # from len(list) ~ 1
+			list[i], list[0] = list[0], list[i]  # swap list[i] and list[0]
 			movedown(list, 0, i)
+
 
 def movedown(list, i, length):
 	val = list[i]
-	while True:
+	while i*2<length-1:
 		c = (i + 1) * 2 - 1
-		if c >= length:
-			break
 
 		if c + 1 < length and list[c + 1] > list[c]:
 			c += 1
@@ -29,7 +27,7 @@ def movedown(list, i, length):
 	list[i] = val
 
 
-def heapify(list): # convert list to max heap
+def heapify(list):  # convert list to max heap
 	length = len(list)
 	for i in xrange(len(list) - 1, -1, -1):  # len(list)-1 ~ 0
 		movedown(list, i, length)
