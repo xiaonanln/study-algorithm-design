5bf6c0b429b1d841c4c5da1ceefecf1ce3c45fac
seis <xiaonanln@gmail.com>
2017-10-02 00:26:35 +0800

    working on cc150

---
+++ src/cc150/Q2.1.py
@@ -0,0 +1,16 @@
+# Write code to remove duplicates from an unsorted linked list.
+#
+# FOLLOW UP
+#
+# How would you solve this problem if a temporary buffer is not allowed?
+
+import utils
+
+def removeDuplicates(head):
+
+	pass
+
+head = utils.makelist(1, 2, 2, 3, 4, 5, 4, 4, 3)
+removeDuplicates(head)
+utils.printlist(head)
+
